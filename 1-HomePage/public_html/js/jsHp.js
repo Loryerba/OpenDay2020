0123e17de0dc7bf7354d983db5ab0294e2c601b4
Loryerba <72017837+Loryerba@users.noreply.github.com>
2020-12-04 22:55:04 +0100

    Half Update

--- 1-HomePage/public_html/js/jsHp.js
+++ 1-HomePage/public_html/js/jsHp.js
@@ -1,8 +1,5 @@
 function checkPromo(){
 	
-	//controllo se ci sono promozioni. Se ci sono promo apro la pagina promo.html
-	//se non ci sono promo apro la pagina nopromo.html
-	window.open("nopromo.html", "_self");
 	
 	window.open("promo.html" , "_self");
 }
